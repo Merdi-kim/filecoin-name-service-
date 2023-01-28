7343da37c84e80d72231ca5cde4a66f5fcecd6a0
Merdi-kim <merdikimuha1@gmail.com>
2023-01-28 18:35:01 +0300

    add new functions to core functions

--- contracts/fns.sol
+++ contracts/fns.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.0;
 contract FNS {
 
     struct NameInfo {
-        string name;
         address owner;
         address secondController;
         bool isValidator;  
         uint64 ttl;
     }
+
+    //uint64 minttl = 1 years;
 
     /**
      *@dev registry for names 
@@ -20,7 +21,7 @@ contract FNS {
    /**
     * @dev registar a new name to the filecoin name service
     */ 
-   function registerName() external payable {
+   function registerName(string memory _name, uint64 ttl ) external payable {
 
    }
 
@@ -31,10 +32,24 @@ contract FNS {
 
    }
 
+   /**
+    * transfer ownership of a name to its new owner
+    */ 
+   function setNewOwner() external {
+
+   }
+
     /**
      * @dev set a second controller for the name in  case the primary controller is obsolete
      */
    function setSecondController() external {
+
+   }
+
+   /**
+    * @dev adds new deadline to ownership of the name
+    */ 
+   function addTtl() external payable {
 
    }
 
