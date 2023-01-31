311efaf0ff4a8d947c67ac0e5b8ee048e460b494
Merdi-kim <merdikimuha1@gmail.com>
2023-01-31 18:45:51 +0300

    set landing page layout

---
+++ web-app/components/footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='flex px-14 justify-between'>
+        <div>
+            <img src="/smile.png" alt="" className='h-24' />
+        </div>
+        <div>
+            <ul>
+                <li>About </li>
+                <li>Carreers</li>
+                <li>FAQ</li>
+                <li>Rules </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
