a0d3d8f5993dd7d30f91ebbbf95545f221729ca9
Merdi-kim <merdikimuha1@gmail.com>
2023-02-03 23:40:21 +0300

    add new icon and refractor results placeholder component

---
+++ web-app/components/ResultPlaceholder.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ResultPlaceholder() {
+  return (
+    <div className='bg-fadedColor min-w-[350px] h-3/4 flex flex-col items-center justify-center rounded-lg'>
+        <p className='text-lg'>Search results will appear here</p>
+        <img src="/icons/search-results.png" alt="" className='h-32 w-32 mt-6'/>
+    </div>
+  )
+}
+
+export default ResultPlaceholder
